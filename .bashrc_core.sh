32c4acff28054374b0d8206169f3fbd0e41cd3dd
James Craig <jamespcraig@gmail.com>
2021-10-22 12:24:28 +0000

    added .bashrc core

---
+++ .bashrc_core.sh
@@ -0,0 +1,43 @@
+
+# -------------------------------------
+# configuration
+# -------------------------------------
+
+# ripgrep as the default source for fzf
+export FZF_DEFAULT_COMMAND='rg --files'
+
+## virtualenvwrapper
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/pyp
+source /home/james/.local/bin/virtualenvwrapper.sh
+
+# -------------------------------------
+# shortcuts
+# -------------------------------------
+
+## restart of apache
+alias apres='sudo systemctl restart apache2'
+
+# launch tmux
+alias t='tmux'
+
+# source of bashrc
+alias so='source .bashrc'
+
+# run python
+alias py='python3'
+
+# change into python virtualenvs
+alias wo='workon'
+alias de='deactivate'
+
+# access to django scripts
+alias pym='python3 manage.py'
+alias sv='python3 manage.py runserver 0.0.0.0:8000'
+
+# edit cp
+alias cpv='cd /home/james/cp && vim'
+
+# run cp server
+alias cps='cd /home/james/cp && workon cp && sv'
