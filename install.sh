b0f4402cd8515a30332867ab71b7771c08208cb1
james <james@local>
2024-03-05 08:43:29 -0500

    enabled nvim on install script

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ ln -sf $DOTFILES/vim/vimrc $HOME/.vim/vimrc
 ln -sf $DOTFILES/vim/spell $HOME/.vim/spell
 
 # Neovim
-# rm -rf $HOME/.config/nvim
-# ln -s $DOTFILES/nvim $HOME/.config/nvim
+rm -rf $HOME/.config/nvim
+ln -s $DOTFILES/nvim $HOME/.config/nvim
 
 # Tmux
 ln -sf $DOTFILES/tmux/.tmux.conf $HOME/.tmux.conf
